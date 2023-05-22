9583fbb24ddc6f85904babcfc11b935f3f6bb842
Yonnji <yonnji@kitsune.one>
2023-05-22 15:13:14 +0300

    add transform flags to frame

--- kphys/core/panda/frame.cxx
+++ kphys/core/panda/frame.cxx
@@ -8,27 +8,47 @@ Frame::Frame() {}
 Frame::~Frame() {
     _bone_names.clear();
     _transforms.clear();
+    _transform_flags.clear();
 }
 
-void Frame::add_transform(char* name, ConstPointerTo<TransformState> transform) {
-  _bone_names.push_back(name);
-  _transforms[name] = transform;
+void Frame::add_transform(
+        const char* name, ConstPointerTo<TransformState> transform,
+        bool has_pos, bool has_hpr, bool has_quat) {
+    std::string s = std::string(name);
+    _bone_names.push_back(s);
+    _transforms[s] = transform;
+    unsigned short flags = 0;
+    if (has_pos)
+        flags |= TRANSFORM_POS;
+    if (has_hpr)
+        flags |= TRANSFORM_HPR;
+    if (has_quat)
+        flags |= TRANSFORM_QUAT;
+    _transform_flags[s] = flags;
 }
 
 unsigned int Frame::get_num_transforms() {
     return _transforms.size();
 }
 
-char* Frame::get_bone_name(unsigned int i) {
-    return _bone_names[i];
+const char* Frame::get_bone_name(unsigned int i) {
+    return _bone_names[i].c_str();
 }
 
 ConstPointerTo<TransformState> Frame::get_transform(unsigned int i) {
     return _transforms[_bone_names[i]];
 }
 
-ConstPointerTo<TransformState> Frame::get_transform(char* name) {
-    if (_transforms.find(name) == _transforms.end())
+ConstPointerTo<TransformState> Frame::get_transform(const char* name) {
+    std::string s = std::string(name);
+    if (_transforms.find(s) == _transforms.end())
         return NULL;
-    return _transforms[name];
+    return _transforms[s];
+}
+
+unsigned short Frame::get_transform_flags(const char* name) {
+    std::string s = std::string(name);
+    if (_transform_flags.find(s) == _transform_flags.end())
+        return 0;
+    return _transform_flags[s];
 }
--- kphys/core/panda/frame.h
+++ kphys/core/panda/frame.h
@@ -9,25 +9,37 @@
 
 #include "kphys/core/panda/types.h"
 
+BEGIN_PUBLISH
+enum TransformComponent {
+    TRANSFORM_POS = 1 << 0,
+    TRANSFORM_HPR = 1 << 1,
+    TRANSFORM_QUAT = 1 << 2,
+};
+END_PUBLISH
+
 
 class EXPORT_CLASS Frame: public TypedReferenceCount {
 PUBLISHED:
     Frame();
     ~Frame();
     unsigned int get_num_transforms();
-    char* get_bone_name(unsigned int i);
+    const char* get_bone_name(unsigned int i);
     ConstPointerTo<TransformState> get_transform(unsigned int i);
-    ConstPointerTo<TransformState> get_transform(char* name);
+    ConstPointerTo<TransformState> get_transform(const char* name);
+    unsigned short get_transform_flags(const char* name);
 
 private:
     unsigned long _iframe;
-    pvector<char*> _bone_names;
-    pmap<char*, ConstPointerTo<TransformState>> _transforms;
+    pvector<std::string> _bone_names;
+    pmap<std::string, ConstPointerTo<TransformState>> _transforms;
+    pmap<std::string, unsigned short> _transform_flags;
 
     static TypeHandle _type_handle;
 
 public:
-    void add_transform(char* name, ConstPointerTo<TransformState> transform);
+    void add_transform(
+        const char* name, ConstPointerTo<TransformState> transform,
+        bool has_pos, bool has_hpr, bool has_quat);
 
     static TypeHandle get_class_type() {
         return _type_handle;
