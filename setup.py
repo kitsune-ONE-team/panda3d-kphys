69d9b40dffd065d134a2264947228bc49bac10b3
Yonnji <yonnji@kitsune.one>
2024-06-21 00:16:30 +0300

    version up

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class kphys_build_ext(build_ext):
 if __name__ == "__main__":
     setuptools.setup(
         name='panda3d-kphys',
-        version='0.6.8',
+        version='0.6.9',
         description='Physics extensions for Panda3D',
         long_description=(
             'Physics extensions for Panda3D '
