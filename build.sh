db64256731f52f715c08769a27f3b36b62ecb285
Yonnji <yonnji@kitsune.one>
2024-08-14 10:13:16 +0300

    fix bullet order, rtti, tests

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-!/bin/sh
+#!/bin/sh
 # build module into SDK
 
 PYTHON_VERSION=3.13t
@@ -10,13 +10,14 @@ cmake -G "Unix Makefiles" \
     -DBULLET_LIBRARY_DIR=/app/opt/bullet/lib \
     -DCMAKE_INSTALL_PREFIX=/app/opt/sdk/lib/python${PYTHON_VERSION}/site-packages \
     -DINSTALL_PY=ON \
+    -DINTERROGATE_INCLUDE_DIR=/app/build/panda3d/interrogate/src/panda3d-interrogate \
     -DMULTITHREADED_BUILD=16 \
-    -DPANDA_BINARY_DIR=/app/opt/sdk/bin \
-    -DPANDA_INCLUDE_DIR=/app/opt/sdk/include \
-    -DPANDA_LIBRARY_DIR=/app/opt/sdk/lib \
-    -DPYTHON_EXECUTABLE=/app/opt/sdk/bin/python${PYTHON_VERSION} \
-    -DPYTHON_INCLUDE_DIR=/app/opt/sdk/include/python${PYTHON_VERSION} \
-    -DPYTHON_LIBRARY=/app/opt/sdk/lib/libpython${PYTHON_VERSION}.so.1.0 \
+    -DPANDA_BINARY_DIR=/app/build/panda3d/interrogate/src/panda3d-interrogate-build/bin \
+    -DPANDA_INCLUDE_DIR=/app/opt/panda3d/include \
+    -DPANDA_LIBRARY_DIR=/app/opt/panda3d/lib \
+    -DPYTHON_EXECUTABLE=/app/opt/python/bin/python${PYTHON_VERSION} \
+    -DPYTHON_INCLUDE_DIR=/app/opt/python/include/python${PYTHON_VERSION} \
+    -DPYTHON_LIBRARY=/app/opt/python/lib/libpython${PYTHON_VERSION}.so.1.0 \
     -DWITH_TESTS=ON \
     ..
 
