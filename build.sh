991556d5467f86d3bfc9754267c8f3263e81508c
Yonnji <yonnji@kitsune.one>
2025-04-27 19:46:40 +0300

    update interrogate path

--- build.sh
+++ build.sh
@@ -6,19 +6,20 @@ PYTHON_VERSION=3.13t
 mkdir -p build
 cd build
 cmake -G "Unix Makefiles" \
-    -DBULLET_INCLUDE_DIR=/app/opt/bullet/include \
-    -DBULLET_LIBRARY_DIR=/app/opt/bullet/lib \
-    -DCMAKE_INSTALL_PREFIX=/app/opt/sdk/lib/python${PYTHON_VERSION}/site-packages \
+    -DBULLET_INCLUDE_DIR=/var/opt/kitsunetsuki-sdk/dist/bullet/include \
+    -DBULLET_LIBRARY_DIR=/var/opt/kitsunetsuki-sdk/dist/bullet/lib \
+    -DCMAKE_INSTALL_PREFIX=/var/opt/kitsunetsuki-sdk/dist/sdk/lib/python${PYTHON_VERSION}/site-packages \
     -DINSTALL_PY=ON \
-    -DINTERROGATE_EXECUTABLE=/app/opt/interrogate/bin/interrogate \
-    -DINTERROGATE_MODULE_EXECUTABLE=/app/opt/interrogate/bin/interrogate_module \
-    -DINTERROGATE_SOURCE_DIR=/app/jenkins/workspace/interrogate-lynx64 \
+    -DINTERROGATE_EXECUTABLE=/var/opt/kitsunetsuki-sdk/dist/interrogate/bin/interrogate \
+    -DINTERROGATE_MODULE_EXECUTABLE=/var/opt/kitsunetsuki-sdk/dist/interrogate/bin/interrogate_module \
+    -DINTERROGATE_SOURCE_DIR=/var/opt/kitsunetsuki-sdk/jenkins/workspace/interrogate-lynx64 \
+    -DINTERROGATE_LIBRARY_DIR=/var/opt/kitsunetsuki-sdk/dist/interrogate/lib \
     -DMULTITHREADED_BUILD=16 \
-    -DPANDA_INCLUDE_DIR=/app/opt/panda3d/include \
-    -DPANDA_LIBRARY_DIR=/app/opt/panda3d/lib \
-    -DPYTHON_EXECUTABLE=/app/opt/python/bin/python${PYTHON_VERSION} \
-    -DPYTHON_INCLUDE_DIR=/app/opt/python/include/python${PYTHON_VERSION} \
-    -DPYTHON_LIBRARY=/app/opt/python/lib/libpython${PYTHON_VERSION}.so.1.0 \
+    -DPANDA_INCLUDE_DIR=/var/opt/kitsunetsuki-sdk/dist/panda3d/include \
+    -DPANDA_LIBRARY_DIR=/var/opt/kitsunetsuki-sdk/dist/panda3d/lib \
+    -DPYTHON_EXECUTABLE=/var/opt/kitsunetsuki-sdk/dist/python/bin/python${PYTHON_VERSION} \
+    -DPYTHON_INCLUDE_DIR=/var/opt/kitsunetsuki-sdk/dist/python/include/python${PYTHON_VERSION} \
+    -DPYTHON_LIBRARY=/var/opt/kitsunetsuki-sdk/dist/python/lib/libpython${PYTHON_VERSION}.so.1.0 \
     -DWITH_TESTS=ON \
     ..
 
